9317ceff5f8c1260f4b4a2be6e2fe33de7ff3759
mgermaine93 <mgermaine93@gmail.com>
2021-02-03 18:06:04 -0500

    Moved playBagpipes() to image instead of container and made styles so that they're more straightforward to explain.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Participants will use this exact link to connect to the track.
+// (AKA works in CodePen as well.)
 const bagpipes = new Audio("https://raw.githubusercontent.com/mgermaine93/wooster-alumni-college-project/main/sounds/wooster-alumni-college-track.mp3");
 
 function playBagpipes() {
